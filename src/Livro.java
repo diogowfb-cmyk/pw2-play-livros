d3f9eacfc03fb30afd7bed204d7ce00704b58866
diogowfb <diogowfb@hotmail.com>
2026-03-16 08:09:34 -0300

    Criação da Classe Livro

---
+++ src/Livro.java
@@ -0,0 +1,13 @@
+import java.time.LocalTime;
+
+public class Livro {
+    //Tipo nomeVariavel;
+    String titulo;
+    String autor;
+    String narrador;
+    String editora;
+    LocalTime duracao;
+    String versao;
+    double preco;
+    String resumo;
+}
